406bf646aa5d42d4dae2b2eb5b85357e767b4960
Lamer217 <males.public@gmail.com>
2022-03-10 14:39:08 +0100

    additional routing for deployment

--- app.js
+++ app.js
@@ -23,6 +23,15 @@ app.use('/api', allRoutes);
 const authRoutes = require('./routes/auth/auth');
 app.use('/auth', authRoutes);
 
+// Additional routing for deployment
+const path = require('path');
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + '/client/build/index.html');
+});
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
